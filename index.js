27261435c4a1405928fb739b21fd54469635e673
Akuna Joshua <akunajoshua@gmail.com>
2024-04-22 01:25:40 +0100

    password encrypted with bcrypt

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // imports the modules
 const express = require("express")
 const cors = require('cors')
+const bcrypt = require("bcryptjs")
 
 // creates the express server
 const app = express()
 // specify port for app
 const port = 8080
+// specify salt variable for bcrypt
+const salt = bcrypt.genSaltSync(10)
 
 //middlewares
 app.use(cors())
@@ -18,7 +21,7 @@ app.get("/api/v1", (req, res)=>{
 app.post("/register", (req, res)=> {
     try{
         const {email, password} = req.body
-        res.json({email, password})
+        res.json({email, password: bcrypt.hashSync(password, salt)})
     } catch (e){
         console.log(e)
     }
