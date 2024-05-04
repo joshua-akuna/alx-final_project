aca99792471f9b748808874b7ba15bacb5915902
Akuna Joshua <akunajoshua@gmail.com>
2024-05-05 00:32:33 +0100

    refactored the login endpoint to account for jsonwebtokens

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bcrypt = require("bcryptjs")
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const jwt = require('jsonwebtoken')
 
 // creates the express server
 const app = express()
@@ -11,13 +12,13 @@ const app = express()
 const port = 8080
 // specify salt variable for bcrypt
 const salt = bcrypt.genSaltSync(10)
+const secret = 'asdfiwre9ewrewrrewjfisefwiwe'
 
 //middlewares
-app.use(cors())
+app.use(cors({credentials:true,origin:'http://localhost:3000'}))
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://akunajoshua:6FtA0VORRARQxQkV@jblog.tkrnbrj.mongodb.net/?retryWrites=true&w=majority&appName=jblog')
-
 
 app.post("/register", async (req, res)=> {
     try{
@@ -26,7 +27,7 @@ app.post("/register", async (req, res)=> {
         const userInfo = await User.create({username, password:hash})
         res.json(userInfo)
     } catch (e){
-        /*console.log(e)*/
+        console.log(e)
         res.status(400).json("invalid username")
     }
 })
@@ -34,8 +35,16 @@ app.post("/register", async (req, res)=> {
 app.post('/login', async (req, res)=>{
     const {username, password} = req.body
     const userInfo = await User.findOne({username})
-    const isValidUser = bcrypt.compareSync(password, userInfo.password)
-    res.json(isValidUser)
+    const isValid = bcrypt.compareSync(password, userInfo.password)
+
+    if(isValid) {
+        jwt.sign({username, id: userInfo._id}, secret, {}, (err, token)=> {
+            if (err) throw err
+            res.cookie('token', token).json('ok')
+        })
+    } else {
+        res.status(400).json('invalid credentials')
+    }
 })
 
 app.listen(port , ()=>{
